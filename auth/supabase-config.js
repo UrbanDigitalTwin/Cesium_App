0b415e4c024540cdf8566b41cb7fe2b85b36008f
Vaishnavimarupaka26 <vaishnavimarupaka02@gmail.com>
2025-07-01 16:29:42 -0400

    updated database

--- auth/supabase-config.js
+++ auth/supabase-config.js
@@ -1,7 +1,7 @@
 // Replace these with your Supabase project config values
 export const supabaseConfig = {
-  supabaseUrl: "YOUR_SUPABASE_URL",
-  supabaseAnonKey: "YOUR_SUPABASE_ANON_KEY",
+  supabaseUrl: "https://tcgkqchumgcwqoylehed.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRjZ2txY2h1bWdjd3FveWxlaGVkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEzOTkwNDEsImV4cCI6MjA2Njk3NTA0MX0.MwsoDqvJwJt0Jxus8WlXcxEKP5ZnnUOJ0aQwVTXIxA8",
 };
 
 // Example configuration (replace with your actual values):
